1a5bc414f3ed1fdc832b046967c2a5d59d39dd92
Wolfgang Jung <w.jung@polyas.de>
2017-01-16 00:41:12 +0100

    Readme added

--- rebuild.sh
+++ rebuild.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
-echo "remove old"
-docker rm $(docker ps -a | grep Exited | awk '{print $1}')
-# docker rm ldap-data
-docker rm postfix-data
-#(cd ldap-schema; docker build -t ldap-data-prepopulate . )
-#cd postfix; docker build -t ldap-data-prepopulate . )
 echo "create data container"
-docker create -v /etc/ldap -v /var/lib/ldap --name ldap-data busybox
-docker create -v /var/spool/postfix --name postfix-data busybox
-export SLAPD_PASSWORD=mysecretpassword
-export SLAPD_DOMAIN=mail.ideas-in-logic.de
+# docker create -v /etc/ldap -v /var/lib/ldap --name ldap-data busybox
+# docker create -v /var/spool/postfix --name postfix-data busybox
+export SLAPD_DOMAIN=i-i-l.de
+export SLAPD_PASSWORD=ulterrednEsSer87
+export BASE_DN='dc=i-i-l,dc=de'
 echo "build images"
 docker-compose build
 echo "Start"
 docker-compose up
-docker logs -f mailserver_openldap_run_1
