87707fc1daa9c830d9fab60ef10be2d4c30b1c38
Wolfgang Jung <w.jung@polyas.de>
2017-01-15 23:26:10 +0100

    Started dovecot delivery

--- dovecot/entrypoint.sh
+++ dovecot/entrypoint.sh
@@ -4,14 +4,13 @@ echo starting dovecot ${POSTFIX_DOMAIN}
 if [ -f /etc/first_start ]; then
     echo fixing environment
 
-
-    find /etc -type f -print0 | \
+    find /etc/dovecot -type f -print0 | \
         xargs -0 perl -p -i -e \
-        "s/___DOMAIN___/${POSTFIX_DOMAIN}/g;
-         s/___BASE_DN___/${BASE_DN}/g;
-         s/___POSTFIX_LDAP_USER___/${POSTFIX_LDAP_USER},${BASE_DN}/g;
-         s/___POSTFIX_LDAP_PASS___/${POSTFIX_LDAP_PASS}/g;
+        "s/___BASE_DN___/${BASE_DN}/g;
+         s/___DOVECOT_LDAP_USER___/${DOVECOT_LDAP_USER},${BASE_DN}/g;
+         s/___DOVECOT_LDAP_PASS___/${DOVECOT_LDAP_PASS}/g;
         "
+
 #    if [ ! -f /var/spool/postfix/etc/dh_2048.pem  ]; then
 #        mkdir -p /var/spool/postfix/etc/
 #        cp /etc/dh_2048.pem /var/spool/postfix/etc/dh_2048.pem
@@ -22,4 +21,6 @@ if [ -f /etc/first_start ]; then
     rm /etc/first_start
 fi;
 
+/etc/init.d/rsyslog start
+/etc/init.d/dovecot start
 /bin/bash
